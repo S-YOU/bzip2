94dde154361af63120bf5513fc5bb1bc62d2280e
S-YOU <S-YOU@users.noreply.github.com>
2015-11-26 09:55:06 +0000

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup, Extension
+
+ext = Extension('bzip2', 
+    sources=['bzip2.c'],
+    extra_compile_args=['-O3', '-std=c99', '-Ofast'],
+    extra_link_args=["-s"],
+    language="c",
+)
+
+setup(
+    name='bzip2', 
+    author='S-YOU', author_email='S-YOU@users.noreply.github.com', 
+    version='0.0.1', 
+    url = 'https://github.com/S-YOU/bzip2',
+    download_url = 'https://github.com/S-YOU/bzip2/tarball/master',
+    ext_modules=[ext]
+)
